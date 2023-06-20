7e2b2f4869b4e012c7ac3e91b020b70bc68cd5f7
TabLine <genricoqui@gmail.com>
2023-06-20 12:49:33 +0000

    Changed end digit variable in if functions to y

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	y = n % 10;
 	printf("Last digit of %d is %d", n, y);
-	if (n > 5)
+	if (y > 5)
 {
 		printf(" and is greater than 5\n");
 }
-	if (n == 0)
+	if (y == 0)
 {
 		printf(" and is 0\n");
 }
-	if ((n < 6) && (n != 0))
+	if ((y < 6) && (y != 0))
 {
 		printf(" and is less than 6 and not 0\n");
 }
 	return (0);
-
+}
