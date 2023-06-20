c1d1a4e6eea5090d7d8891f4120aafa937675429
TabLine <genricoqui@gmail.com>
2023-06-20 12:37:22 +0000

    Attempted to fix output for n equals to 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,16 +18,11 @@ int main(void)
 	y = n % 10;
 	printf("Last digit of %d is %d", n, y);
 	if (n > 5)
-{
 		printf(" and is greater than 5\n");
-}
 	if (n == 0)
-{
 		printf(" and is 0\n");
-}
 	if ((n < 6) && (n != 0))
-{
 		printf(" and is less than 6 and not 0\n");
-}
+
 	return (0);
 }
